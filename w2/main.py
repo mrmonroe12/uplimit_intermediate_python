627fdcd0cd8d3c08d5d9eed10cf648341741e2de
Mike Monroe <mrmonroe12@gmail.com>
2024-03-12 10:29:44 +0000

    Completed all week 2 work, ran tests on test and small data sets

--- w2/main.py
+++ w2/main.py
@@ -10,6 +10,7 @@ import json
 import argparse
 from datetime import datetime
 from pprint import pprint
+import math
 
 CURRENT_FOLDER_NAME = os.path.dirname(os.path.abspath(__file__))
 
@@ -63,17 +64,16 @@ def get_sales_information(file_path: str) -> Dict:
 # batches the files based on the number of processes
 def batch_files(file_paths: List[str], n_processes: int) -> List[set]:
     if n_processes > len(file_paths):
-        return #### [YOUR CODE HERE] ####
+        return [set(path) for path in file_paths]
 
-    n_per_batch = #### [YOUR CODE HERE] ####
+    n_per_batch = math.floor(len(file_paths)/n_processes)
 
     first_set_len = n_processes * n_per_batch
     first_set = file_paths[0:first_set_len]
-    second_set = #### [YOUR CODE HERE] ####
+    second_set = file_paths[first_set_len:]
 
     batches = [set(file_paths[i:i + n_per_batch]) for i in range(0, len(first_set), n_per_batch)]
-    for ind, each_file in enumerate(second_set):
-        #### [YOUR CODE HERE] ####
+    batches[0] = batches[0].union(set(second_set))
 
     return batches
 
@@ -144,7 +144,8 @@ def main() -> List[Dict]:
     """
 
     st = time.time()
-    n_processes = 3 # you may modify this number - check out multiprocessing.cpu_count() as well
+    n_processes = 7 # you may modify this number - check out multiprocessing.cpu_count() as well
+    print('CPUS:',multiprocessing.cpu_count)
 
     parser = argparse.ArgumentParser(description="Choose from one of these : [tst|sml|bg]")
     parser.add_argument('--type',
@@ -162,24 +163,32 @@ def main() -> List[Dict]:
     file_paths = [os.path.join(data_folder_path, file_name) for file_name in files]
 
     batches = batch_files(file_paths=file_paths, n_processes=n_processes)
+    print('batches:',len(batches),'n_processes:',n_processes)
 
     ######################################## YOUR CODE HERE ##################################################
     with multiprocessing.Pool(processes=n_processes) as pool:
-        
+        revenue_data = pool.starmap(run,[(file_batch, batch_num) for batch_num, file_batch in enumerate(batches)])
+        pool.close()
+        pool.join()
+
+    revenue_data = revenue_data[0]
     ######################################## YOUR CODE HERE ##################################################
 
     en = time.time()
     print("Overall time taken : {}".format(en-st))
-
+    print(revenue_data)
     ######################################## YOUR CODE HERE ##################################################
     for yearly_data in revenue_data:
-        
+        with open(os.path.join(output_save_folder, f'{yearly_data["file_name"]}.json'), 'w') as f:
+            f.write(json.dumps(yearly_data))
+
+        plot_sales_data(yearly_revenue=yearly_data['revenue_per_region'], year=yearly_data["file_name"],
+                        plot_save_path=os.path.join(output_save_folder, f'{yearly_data["file_name"]}.png'))
 
     ######################################## YOUR CODE HERE ##################################################
         
     # should return revenue data
-    return #### [YOUR CODE HERE] ####
-
+    return revenue_data
 
 if __name__ == '__main__':
     res = main()
